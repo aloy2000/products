bf2ed5d2536db6a6698e0040aeaa4e280cdc2384
aloy2000 <ramanandraibenirina@gmail.com>
2023-03-16 20:39:26 +0300

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8030326f93cc814"; }
+/******/ 		__webpack_require__.h = function() { return "87b6ac6db89f5a02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
